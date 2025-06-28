650319fd27953e0f9ff01af1d705e1aa33fb3349
Jothikrishnan <jothikrishnan2015@gmail.com>
2025-06-28 20:07:59 +0530

    varibale.js added

---
+++ variable.js
@@ -0,0 +1,28 @@
+//variable declaration
+let bookName;
+
+// variable Initialization
+bookName = "The Gentleman 1964";
+
+let bookAuthor = "Tatayya Naidu"
+    bookPubliser =" bookers shop"
+console.log(bookAuthor, "\n", bookPubliser);
+// \n is a escape sequence which is going to the next line//
+
+
+var carName  ="zzz";
+bookName= "yyy";
+console.log(carName);
+
+// rules to naming the variable
+// it should be unique
+// should not start with integer, but can have digit
+// never use the reserved work,eg keywords
+// case sensitive 
+
+//asssingment
+let student, sname;
+
+sname= "jothikrishnan" ;
+student= sname;
+console.log(sname);
